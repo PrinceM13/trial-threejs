7b905f01796e01f5d32455aea5555af8595ce39d
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-08-24 18:35:40 +0700

    feat(home): add motion and content for home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Customizer, Home } from "@/pages";
 export default function App() {
   return (
     <main className="app transition-all ease-in">
-      <h1 className="head-text">ThreeJS</h1>
       <Home />
       <Canvas />
       <Customizer />
---
+++ components/Button/Custom/index.tsx
@@ -0,0 +1,50 @@
+import state from "@/store";
+import { useSnapshot } from "valtio";
+
+const ButtonCustom = ({
+  type,
+  title,
+  customStyles,
+  handleClick
+}: {
+  type: string;
+  title: string;
+  customStyles: string;
+  handleClick: () => void;
+}): JSX.Element => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type: string) => {
+    switch (type) {
+      case "filled":
+        return {
+          backgroundColor: snap.color,
+          color: "#fff"
+        };
+
+      //   case "outlined":
+      //     return {
+      //       backgroundColor: "transparent",
+      //       color: snap.color,
+      //       border: `1px solid ${snap.color}`
+      //     };
+
+      default:
+        return {
+          backgroundColor: "#5F9EA0",
+          color: "#fff"
+        };
+    }
+  };
+  return (
+    <button
+      className={"px-2 py-1.5 flex-1 rounded-md ${customStyles}"}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default ButtonCustom;
